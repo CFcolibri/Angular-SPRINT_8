82a82d5de6b7bbf6d2784a65a124e2be94e028cc
CFcolibri <carlotafigueras@gmail.com>
2023-06-09 14:25:37 +0200

    starships-compoent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+import { StarshipsComponent } from './starships/starships.component';
+import { NgxSpinnerModule } from 'ngx-spinner';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     WelcomePageComponent,
+    StarshipsComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    NgxSpinnerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
