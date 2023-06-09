3bee45b5c1364f678fb292cecef4c4a91b4a7294
CFcolibri <carlotafigueras@gmail.com>
2023-06-09 12:08:57 +0200

    start-over

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,21 +5,18 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { ChildShipComponent } from './child-ship/child-ship.component';
-import { StarshipService } from './starship.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ChildShipComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule
   ],
-  providers: [StarshipService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,6 +1,4 @@
-
 import { Component } from '@angular/core';
-import { StarshipService } from '../starship.service';
 
 @Component({
   selector: 'app-header',
@@ -8,17 +6,5 @@ import { StarshipService } from '../starship.service';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-  filteredStarships: any[] = [];
 
-  constructor(private starshipService: StarshipService) {}
-
-  onClickStarshipButton(): void {
-    this.starshipService.getStarships().subscribe(data => {
-      this.filteredStarships = data.results;
-    });
-  }
-
-  receiveFilteredStarships(filteredStarships: any[]): void {
-    this.filteredStarships = filteredStarships;
-  }
 }
